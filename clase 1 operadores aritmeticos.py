d342feb064b7e56301c8ec2921c4f926d6b342cc
OGBVS <bastian.delgado@alumnos.upla.cl>
2022-04-13 17:06:33 -0400

    999 CLASE1 FRANJA
    999 CLASE 1 DE FRANJA PYTHON
    
    PROF EZE LAGOS

---
+++ clase 1 operadores aritmeticos.py
@@ -0,0 +1,27 @@
+from math import sqrt
+
+
+print("suma ")
+print(2 + 3)
+
+print("resta ")
+print(2 - 3)
+
+print("multiplicacion")
+print(2 * 3)
+
+print("division")
+print(9 // 3)
+
+print("division modulo")
+print( 9 % 3)
+
+print("exponente")
+print( 2 ** 8)
+
+print("raiz cuadrada")
+print( sqrt(4) )
+
+#**REGLA DE PROCEDENCIA**#
+print(" ** OPERADOR MATEMATICO ** ")
+print( 5 + (2*5) )
